afb65fefee538f51172e5a7fe910460f51a5778d
Jérémy Gendre <jgendre@media-sante.com>
2020-10-15 09:36:23 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            gradientColorStops: {
+                "back-light": "#27427b",
+                "back-dark": "#111838",
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
