0a459754adaeab23626c45c3434afb10a84fee0a
Jérémy Gendre <jgendre@media-sante.com>
2020-10-15 16:38:02 +0200

    manage the form in localstorage (save the progression)

--- src/page/FormPage.tsx
+++ src/page/FormPage.tsx
@@ -1,16 +1,25 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {QCM} from "../Data";
 import DisplayQuestion from "../component/DisplayQuestion";
 import {Response} from "../type/Response";
+import {useSession} from "../context/SessionContext";
 
 export default function FormPage(){
     const [currentQuestionIndex,setCurrentQuestionIndex] = useState<number>(0);
+    const session = useSession();
 
     const handleNext = (response:Response) => {
         //TODO : request API send
         console.log(response);
+        session.updateSession('formStep',currentQuestionIndex + 1);
         setCurrentQuestionIndex(currentQuestionIndex + 1);
     };
+
+    useEffect(() => {
+        if(session.formStep !== undefined){
+            setCurrentQuestionIndex(session.formStep);
+        }
+    },[])
     return (
         <div className="m-10 w-full">
             <div className="text-right italic text-lg">Question {currentQuestionIndex + 1}/{QCM.length}</div>
