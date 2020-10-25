0d4570ce0ee4e77d2095b39497bb5d07d1b5ec97
Mathieu TUDISCO <oss@mathieutu.dev>
2020-10-25 11:04:38 +0100

    Simplify App

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,31 @@
 import React from 'react'
 import './assets/css/App.css'
 import Header from './component/Header'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import HomePage from './page/HomePage'
 import FormPage from './page/FormPage'
 import ErrorPage from './page/ErrorPage/ErrorPage'
 import { useSession } from './context/SessionContext'
 import LoginPage from './page/LoginPage'
-import { ApolloProvider } from './context/Apollo'
 
 
-function App() {
+export const App = () => {
   const { state } = useSession()
+
+  if (!state.user) {
+    return <LoginPage />
+  }
+
   return (
-    <ApolloProvider>
-      <BrowserRouter>
-        {state.user === null ? (
-          <LoginPage />
-        ) : (
-          <>
-            <Header />
-            <div className="container shadow-lg flex mx-auto bg-white h-full">
-              <Switch>
-                <Route exact path="/"><HomePage /></Route>
-                <Route exact path="/form"><FormPage /></Route>
-                <Route><ErrorPage code={404} message="Not Found" /></Route>
-              </Switch>
-            </div>
-          </>
-        )}
-      </BrowserRouter>
-    </ApolloProvider>
+    <>
+      <Header />
+      <div className="container shadow-lg flex mx-auto bg-white h-full">
+        <Switch>
+          <Route exact path="/"><HomePage /></Route>
+          <Route exact path="/form"><FormPage /></Route>
+          <Route><ErrorPage code={404} message="Not Found" /></Route>
+        </Switch>
+      </div>
+    </>
   )
 }
-
-export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './assets/css/index.output.css'
-import App from './App'
+import { App } from './App'
 import AppSessionProvider from './context/SessionContext'
+import { ApolloProvider } from './context/Apollo'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
+const root = (
   <React.StrictMode>
     <AppSessionProvider>
-      <App />
+      <ApolloProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ApolloProvider>
     </AppSessionProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 )
+
+ReactDOM.render(root, document.getElementById('root'))
