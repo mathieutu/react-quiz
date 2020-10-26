ef9818c809df0e546dc1bc8dc043389ccda238fa
Mathieu TUDISCO <oss@mathieutu.dev>
2020-10-26 15:32:28 +0100

    Improver loader on button

--- src/component/FormHeader.tsx
+++ src/component/FormHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Timer } from './Timer'
 import { LoadingIcon } from './LoadingIcon'
 import { useQuiz } from '../context/QuizContext'
@@ -9,8 +9,26 @@ type FormHeaderProps = {
   loading: boolean,
 }
 
-function PreviousButton({ disabled, onClick }: { onClick: () => void, disabled: boolean }) {
+const useShouldLoad = (loading: boolean, onClick: () => void) => {
+  const [hasClicked, setHasClicked] = useState(false)
+
+  useEffect(() => {
+    if (!loading) {
+      setHasClicked(false)
+    }
+  }, [loading])
+
+  const shouldLoad = hasClicked && loading
+  const handleClick = () => {
+    setHasClicked(true)
+    onClick()
+  }
+
+  return { shouldLoad, handleClick }
+}
+function PreviousButton({ loading, onClick }: { onClick: () => void, loading: boolean }) {
   const { currentQuestionIndex } = useQuiz()
+  const { handleClick, shouldLoad } = useShouldLoad(loading, onClick)
 
   if (!currentQuestionIndex) {
     return null
@@ -20,11 +38,11 @@ function PreviousButton({ disabled, onClick }: { onClick: () => void, disabled: 
     <span className="shadow-sm rounded-md">
       <button
         type="button"
-        onClick={onClick}
-        disabled={disabled}
+        onClick={handleClick}
+        disabled={loading}
         className="inline-flex items-center px-4 py-2 border border-gray-300 text-xs sm:text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 transition duration-150 ease-in-out"
       >
-        <LoadingIcon loading={disabled} className="-ml-1 mr-2 h-5 w-5 text-gray-500">
+        <LoadingIcon loading={shouldLoad} className="-ml-1 mr-2 h-5 w-5 text-gray-500">
           <svg className="-ml-1 mr-2 h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
           </svg>
@@ -35,25 +53,29 @@ function PreviousButton({ disabled, onClick }: { onClick: () => void, disabled: 
   )
 }
 
-type NextButtonProps = { onClick: () => void, disabled: boolean, isLastQuestion: boolean }
+type NextButtonProps = { onClick: () => void, loading: boolean, isLastQuestion: boolean }
 
-const NextButton = ({ disabled, onClick, isLastQuestion }: NextButtonProps) => (
-  <span className="shadow-sm rounded-md">
-    <button
-      type="button"
-      onClick={onClick}
-      disabled={disabled}
-      className="inline-flex items-center px-4 py-2 border border-transparent text-xs sm:text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out"
-    >
-      {isLastQuestion ? 'Terminer' : 'Question suivante'}
-      <LoadingIcon loading={disabled} className="-mr-1 ml-2 h-5 w-5">
-        <svg className="-mr-1 ml-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-        </svg>
-      </LoadingIcon>
-    </button>
-  </span>
-)
+const NextButton = ({ loading, onClick, isLastQuestion }: NextButtonProps) => {
+  const { handleClick, shouldLoad } = useShouldLoad(loading, onClick)
+
+  return (
+    <span className="shadow-sm rounded-md">
+      <button
+        type="button"
+        onClick={handleClick}
+        disabled={loading}
+        className="inline-flex items-center px-4 py-2 border border-transparent text-xs sm:text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-700 active:bg-indigo-700 transition duration-150 ease-in-out"
+      >
+        {isLastQuestion ? 'Terminer' : 'Question suivante'}
+        <LoadingIcon loading={shouldLoad} className="-mr-1 ml-2 h-5 w-5">
+          <svg className="-mr-1 ml-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        </LoadingIcon>
+      </button>
+    </span>
+  )
+}
 
 export const FormHeader = ({ loading, onPrevious, onNext }: FormHeaderProps) => {
   const { currentQuestionIndex, questionsQuantity } = useQuiz()
@@ -82,8 +104,8 @@ export const FormHeader = ({ loading, onPrevious, onNext }: FormHeaderProps) => 
         </div>
       </div>
       <div className="mt-5 flex flex-wrap gap-3 lg:mt-0 lg:ml-4">
-        <PreviousButton onClick={onPrevious} disabled={loading} />
-        <NextButton onClick={onNext} disabled={loading} isLastQuestion={questionsLeftQuantity === 1} />
+        <PreviousButton onClick={onPrevious} loading={loading} />
+        <NextButton onClick={onNext} loading={loading} isLastQuestion={questionsLeftQuantity === 1} />
       </div>
     </header>
   )
